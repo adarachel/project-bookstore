87ac53588063a740a4daea8c1df4a24e94833aa2
adarachel <adarachelofficial@gmail.com>
2023-04-15 16:10:49 +0100

    initialize components

---
+++ src/routes/Categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CategoriesPage = ({ books, onDelete }) => {
+  return (
+    <div>
+      <h1>Categories Page</h1>
+      {/* Add categories page content */}
+    </div>
+  );
+};
+
+export default CategoriesPage;
