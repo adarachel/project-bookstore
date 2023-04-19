d33aef29d67e5a294f5844327f53bf86b0be3065
adarachel <adarachelofficial@gmail.com>
2023-04-19 19:09:47 +0100

    fix errors

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,8 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  categories: ['Under construction'],
-};
+const initialState = [];
 
 const categoriesSlice = createSlice({
   name: 'categories',
