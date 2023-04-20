435a00bcf91902d1af9385282b2310a4e599caa9
adarachel <adarachelofficial@gmail.com>
2023-04-20 17:26:31 +0100

    Use Redux in React components

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import './BooksContainer.css';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
-const BookForm = () => (
-  <div>
-    <h2 className="form-title">Add Your Book</h2>
-    <section>
-      <form className="Add-form">
-        <input className="input title-input" type="text" name="title" placeholder="Add Book Title" />
-        <input className="input" type="text" name="author" placeholder="Add Book Author" />
-        <button className="primary-button-big" type="submit">ADD BOOK</button>
-      </form>
-    </section>
-  </div>
-);
+const BookForm = () => {
+  const dispatch = useDispatch();
+
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const author = e.target.author.value;
+    const id = Math.floor(Math.random() * 100000);
+    dispatch(addBook({ id, title, author }));
+    e.target.reset();
+  };
+
+  return (
+    <div>
+      <h2 className="form-title">Add Your Book</h2>
+      <section>
+        <form className="Add-form" onSubmit={handleAddBook}>
+          <input className="input title-input" type="text" name="title" placeholder="Add Book Title" />
+          <input className="input" type="text" name="author" placeholder="Add Book Author" />
+          <button className="primary-button-big" type="submit">ADD BOOK</button>
+        </form>
+      </section>
+    </div>
+  );
+};
 
 export default BookForm;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,23 +2,23 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = [
   {
-    "item_id": "item1",
-    "title": "The Great Gatsby",
-    "author": "John Smith",
-    "category": "Fiction"
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
   },
   {
-    "item_id": "item2",
-    "title": "Anna Karenina",
-    "author": "Leo Tolstoy",
-    "category": "Fiction"
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
   },
   {
-    "item_id": "item3",
-    "title": "The Selfish Gene",
-    "author": "Richard Dawkins",
-    "category": "Nonfiction"
-  }
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
 ];
 
 export const booksSlice = createSlice({
@@ -27,11 +27,11 @@ export const booksSlice = createSlice({
   reducers: {
     addBook: (state, action) => {
       const { id, title, author } = action.payload;
-      state.push({ item_id: id, title, author });
+      state.push({ id, title, author });
     },
     removeBook: (state, action) => {
-      const id = action.payload;
-      return state.filter(book => book.item_id !== id);
+      const idToRemove = action.payload;
+      return state.filter((book) => book.id !== idToRemove);
     },
   },
 });
